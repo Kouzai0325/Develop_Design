a2577971d0f20a3d3e1290cc5ae6f50fd23fe437
Kouzai0325 <2104240013ka@edu.oca.ac.jp>
2023-12-02 16:02:46 +0900

    lambda

--- try_lambda.py
+++ try_lambda.py
@@ -1,15 +1,15 @@
 def execute_param_fn(args, hosei, fn):
-    result = fn(args[0], args[1])
-    result_with_correction = result + hosei
-    return result_with_correction
+    result = fn(args, hosei)
+    
+    return result
 
 
 # ラムダ関数1
-lambda_fn1 = lambda a, b: max(a, b)
-
+lambda_fn1 = lambda a, b: max(a) + b
+ 
 
 # ラムダ関数2
-lambda_fn2 = lambda a, b: min(a, b)
+lambda_fn2 = lambda a, b: min(a) - b
 
 
 result1 = execute_param_fn([1, 2, 3], 4, lambda_fn1)
